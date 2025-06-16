9dffcc84ffab92985642b351cdf979c39d26a123
Chris Miskowiec <cmiskowiec@precisionlender.com>
2025-06-16 13:31:46 -0500

    Adds plugins and editorconfig

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      size = 10,
+      open_mapping = [[<A-t>]],
+      hide_numbers = true,
+      autochdir = true,
+      start_in_insert = true,
+      insert_mappings = false,
+      terminal_mappings = true,
+      direction = 'horizontal',
+      close_on_exit = true,
+    },
+  },
+}
